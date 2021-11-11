63ec5c9001db499b69a162410c7ae294d5033168
AntonGoncharik <ant.goncharik@gmail.com>
2021-11-11 23:34:29 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx']
+  },
+  externals: {
+    react: 'react'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: ['ts-loader'],
+        exclude: ['/node_modules']
+      },
+      {
+        test: /\.module\.(scss|sass)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-modules-typescript-loader",
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: 'style_[local]__[hash:base64:5]',
+              }
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env"
+                  ],
+                ],
+              },
+            },
+          },
+          "sass-loader",
+        ],
+        exclude: ['/node_modules']
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+  ],
+}
